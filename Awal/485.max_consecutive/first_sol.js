78690c51a0ec9e8ed53122f2311c76e2e05bfcd5
Awal hossain <waqin21@gmail.com>
2022-10-27 12:14:05 +0600

    max consecutive one solved 🚀🚀

---
+++ Awal/485.max_consecutive/first_sol.js
@@ -0,0 +1,16 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let max = 0;
+    let newNum=0;
+    for(let i=0;i<nums.length; i++){
+
+        if(nums[i]==0){
+            console.log(newNum,"chek")
+            max = Math.max(max, newNum);
+             newNum =0;
+        }else{
+            newNum++
+        }
+         max = Math.max(max, newNum);
+    }
+        return max;
+};
