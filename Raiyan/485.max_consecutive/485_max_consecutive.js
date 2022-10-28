641f82a672c8c286eab7f7840d66227e176c81b7
Raiyan <tajkierhaque@gmail.com>
2022-10-28 11:36:38 +0600

    Max consecutive ones solved using a optimized solution

--- Raiyan/485.max_consecutive/485_max_consecutive.js
+++ Raiyan/485.max_consecutive/485_max_consecutive.js
@@ -1,5 +1,6 @@
 // # 485 Max Consecutive Ones
 
+// method - 1
 function findMaxConsecutiveOnes(nums) {
     let maxConsecutive = 0;
     const array = [];
@@ -15,4 +16,20 @@ function findMaxConsecutiveOnes(nums) {
     return array.sort((a, b) => b - a)[0] || 0;
 }
 
-console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
+// method - 2
+function findMaxConsecutiveOnes2(nums) {
+    let max = 0;
+    let newMax = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            newMax++;
+        } else {
+            max = Math.max(max, newMax);
+            newMax = 0;
+        }
+    }
+    return Math.max(max, newMax);
+}
+
+console.log(findMaxConsecutiveOnes2([1, 1, 0, 1, 1, 1]));
