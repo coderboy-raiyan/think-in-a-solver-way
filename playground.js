26723e9cc0edd0201813fedde7b688c6559cc9fe
Sadique <mhsadique1307@gmail.com>
2022-10-29 15:05:14 +0600

    1089 solved with JS built-in method

--- playground.js
+++ playground.js
@@ -1,22 +1,12 @@
-// Find Numbers with Even Number of Digits
-// Given an array nums of integers, return how many of them contain an even number of digits.
+function duplicateZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i]) {
+            arr.splice(i + 1, 0, 0)
+            arr.pop()
+            i++
+        }
+    }
+    return arr
+}
 
-// Example 1:
-// Input: nums = [12,345,2,6,7896]
-// Output: 2
-// Explanation:
-// 12 contains 2 digits (even number of digits).
-// 345 contains 3 digits (odd number of digits).
-// 2 contains 1 digit (odd number of digits).
-// 6 contains 1 digit (odd number of digits).
-// 7896 contains 4 digits (even number of digits).
-// Therefore only 12 and 7896 contain an even number of digits.
-// Example 2:
-// Input: nums = [555,901,482,1771]
-// Output: 1
-// Explanation:
-// Only 1771 contains an even number of digits.
-
-// Constraints:
-// 1 <= nums.length <= 500
-// 1 <= nums[i] <= 105
+console.log(duplicateZero([1,0,2,3,0,4,5,0]));
