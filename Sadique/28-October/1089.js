9cc22d04554303176f10f9c5edba2280001c26a8
Sadique <mhsadique1307@gmail.com>
2022-10-29 16:29:23 +0600

    1089 solved without using any built-in methods

--- Sadique/28-October/1089.js
+++ Sadique/28-October/1089.js
@@ -17,6 +17,7 @@
 
 // Show hints to understand more about this question
 
+// Method 01
 function duplicateZero(arr) {
     function duplicateZero(arr) {
         for (let i = 0; i < arr.length; i++) {
@@ -28,6 +29,47 @@ function duplicateZero(arr) {
         }
         return arr
     }
+}
+
+
+// Method 02
+// without using any built-in method
+function duplicateZero2(arr) {
+    let zeros = 0
+	let c1 = arr.length - 1
+	let c2 = arr.length - 1
+
+	for (let a of arr) {
+		if (a === 0) {
+			zeros++
+		}
+	}
+
+	c2 += zeros
+		
+	while (c1 >= 0) {
+		if (arr[c1] === 0) {
+			if (c2 < arr.length) {
+				arr[c2] = arr[c1]
+			}
+
+			c2--
+
+			if (c2 < arr.length) {
+				arr[c2] = arr[c1]
+			}
+		}
+		else {
+			if (c2 < arr.length) {
+				arr[c2] = arr[c1]
+			}
+		}
+
+		c1--
+		c2--
+	}
+
+    return arr
 }
 
 console.log(duplicateZero([1,0,2,3,0,4,5,0]));
