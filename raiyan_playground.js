e7a2a159baad63f9cfb3d5713569d73ecdaacb2e
Raiyan <tajkierhaque@gmail.com>
2022-11-02 18:24:09 +0600

    valid Mountain array practiced

--- raiyan_playground.js
+++ raiyan_playground.js
@@ -13,5 +13,35 @@ function maxConsecutive(nums) {
     }
     return Math.max(maxNum, previousMax);
 }
+maxConsecutive([1, 1, 0, 1, 1, 1]);
 
-console.log(maxConsecutive([1, 1, 0, 1, 1, 1]));
+// 941. Valid Mountain Array
+function validMountainArray(arr) {
+    let startValley = 0;
+    if (arr.length < 3) {
+        return false;
+    }
+
+    if (arr[1] < arr[0]) {
+        return false;
+    }
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] === arr[i]) {
+            return false;
+        }
+        if (arr[i - 1] > arr[i]) {
+            startValley = i - 1;
+            break;
+        }
+    }
+
+    for (let i = startValley; i <= arr.length - 1; i++) {
+        if (arr[i] <= arr[i + 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+console.log(validMountainArray([0, 3, 2, 1]));
