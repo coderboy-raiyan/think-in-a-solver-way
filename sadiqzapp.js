f581ab0ba0188a76f9ae2c99b9a0ba221bf93e5a
mhSadique <mhsadique1307@gmail.com>
2022-11-06 13:29:39 +0600

    88 solved with 2 pointers

--- sadiqzapp.js
+++ sadiqzapp.js
@@ -1,16 +1,20 @@
-var merge = function(nums1, m, nums2, n) {
-    let nums2Pointer = 0
-    let lastZeroIndex = 0
-    for (let i = 0; i < nums1.length; i++) {
-        if (nums1[i] === 0 && i >= m) {
-            const currentZeroIndex = nums1.indexOf(0, lastZeroIndex)
-            lastZeroIndex = currentZeroIndex
-            nums1.splice(currentZeroIndex, 1, nums2[nums2Pointer++])
+var merge2 = function(nums1, m, nums2, n) {
+    while(m > 0 && n > 0) {
+        let last = m + n - 1
+        if (nums1[m - 1] >= nums2[n - 1]) {
+            nums1[last] = nums1[m - 1]
+            m--
+        } else {
+            nums1[last] = nums2[n - 1]
+            n--
         }
     }
-    nums1.sort((a, b) => a - b)
 
+    while (n > 0) {
+        nums1[n-1] = nums2[n-1];
+        n--;
+    }
+    
     return nums1
 };
-
-console.log(merge([-1,0,0,3,3,3,0,0,0], 6, [1,2,2], 3));
+console.log(merge2([-1,0,0,3,3,3,0,0,0], 6, [1,2,2], 3));
