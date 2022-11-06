ea863a41e3645f2e10fa77d918b8c6a045a8f21e
Raiyan <tajkierhaque@gmail.com>
2022-11-06 19:13:26 +0600

    1295 revision done

--- raiyan_playground.js
+++ raiyan_playground.js
@@ -13,3 +13,16 @@ function maxConsecutive(nums) {
     }
     return Math.max(max, previousMax);
 }
+
+// 1295. Find Numbers with Even Number of Digits
+
+function evenNumberCount(nums) {
+    let evenCount = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i].toString().length % 2 === 0) {
+            evenCount++;
+        }
+    }
+    return evenCount;
+}
