374e5985e900bf9bf37dff8277502a80f32ee369
Raiyan <tajkierhaque@gmail.com>
2022-11-06 19:09:23 +0600

    485 revision done

--- raiyan_playground.js
+++ raiyan_playground.js
@@ -1,47 +1,15 @@
-// #485 Max Consecutive Ones
+// 485. Max Consecutive Ones
 function maxConsecutive(nums) {
-    let maxNum = 0;
+    let max = 0;
     let previousMax = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 1) {
-            maxNum = Math.max(maxNum, previousMax);
-            previousMax = 0;
-        } else {
+        if (nums[i] !== 0) {
             previousMax++;
+        } else {
+            max = Math.max(max, previousMax);
+            previousMax = 0;
         }
     }
-    return Math.max(maxNum, previousMax);
+    return Math.max(max, previousMax);
 }
-maxConsecutive([1, 1, 0, 1, 1, 1]);
-
-// 941. Valid Mountain Array
-function validMountainArray(arr) {
-    let startValley = 0;
-    if (arr.length < 3) {
-        return false;
-    }
-
-    if (arr[1] < arr[0]) {
-        return false;
-    }
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i - 1] === arr[i]) {
-            return false;
-        }
-        if (arr[i - 1] > arr[i]) {
-            startValley = i - 1;
-            break;
-        }
-    }
-
-    for (let i = startValley; i <= arr.length - 1; i++) {
-        if (arr[i] <= arr[i + 1]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-console.log(validMountainArray([0, 3, 2, 1]));
