eba953a2e949c2abaf3cd7cc61865f6bfbab8d5c
Raiyan <tajkierhaque@gmail.com>
2022-11-09 11:50:41 +0600

    31 october revision done

--- raiyan_playground.js
+++ raiyan_playground.js
@@ -1,52 +1,29 @@
-// 485. Max Consecutive Ones
-function maxConsecutive(nums) {
-    let max = 0;
-    let previousMax = 0;
-
+// 283. Move Zeroes
+function moveZeroes1(nums) {
+    let left = 0;
+    let right = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            previousMax++;
-        } else {
-            max = Math.max(max, previousMax);
-            previousMax = 0;
+        if (nums[left] !== 0) {
+            [nums[right], nums[left]] = [nums[left], nums[right]];
+            right++;
         }
+        left++;
     }
-    return Math.max(max, previousMax);
+    return nums;
 }
 
-// 1295. Find Numbers with Even Number of Digits
-function evenNumberCount(nums) {
-    let evenCount = 0;
+// 905. Sort Array By Parity
+
+function sortArrayByParity(nums) {
+    let left = 0;
+    let right = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i].toString().length % 2 === 0) {
-            evenCount++;
+        if (nums[i] % 2 === 0) {
+            [nums[right], nums[left]] = [nums[left], nums[right]];
+            right++;
         }
+        left++;
     }
-    return evenCount;
+    return nums;
 }
-// 977. Squares of a Sorted Array
-
-// function sortedArray(nums) {
-//     let left = 0
-//     let right = nums.length - 1
-//     let trackIndex = nums.length - 1
-//     const newArray = [];
-
-//        while(right > left){
-//         let leftVal = Math.pow(nums[left], 2)
-//         let rightVal = Math.pow(nums[right], 2)
-
-//         if(leftVal > right){
-//             newArray[trackIndex] = leftVal
-//             leftVal++
-//         }else{
-//             newArray[trackIndex] = rightVal
-//             ++
-//         }
-//        }
-
-//     return newArray
-// }
-
-// console.log(sortedArray([-4, -1, 0, 3, 10]));
