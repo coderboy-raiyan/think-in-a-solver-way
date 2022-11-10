a22434bb577d9f3b1e6aeae6a40a1ffc3970a2df
Raiyan <tajkierhaque@gmail.com>
2022-11-10 11:53:08 +0600

    valid subsequence of an array solved

--- raiyan_playground.js
+++ raiyan_playground.js
@@ -27,3 +27,19 @@ function sortArrayByParity(nums) {
     }
     return nums;
 }
+
+// Is valid Sub sequence of an array Solved
+function isValidSubsequence(array, sequence) {
+    let mainPointer = 0;
+    let subPointer = 0;
+
+    while (mainPointer < array.length && subPointer < sequence.length) {
+        if (array[mainPointer] === sequence[subPointer]) {
+            subPointer++;
+        }
+        mainPointer++;
+    }
+    return subPointer === sequence.length;
+}
+
+console.log(isValidSubsequence([1, 1, 6, 1], [1, 1, 1, 6]));
