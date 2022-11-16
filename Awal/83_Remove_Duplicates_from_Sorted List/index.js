f9137b9298debe83e7c5cd7b945bfb049df43e82
Awal hossain <waqin21@gmail.com>
2022-11-16 22:09:50 +0600

    remove duplicate from sorted array 🚀

---
+++ Awal/83_Remove_Duplicates_from_Sorted List/index.js
@@ -0,0 +1,18 @@
+var deleteDuplicates = function(head) {
+    let curr = head;
+        
+        while(curr){
+            while(curr.next && curr.val === curr.next.val){
+                   curr.next = curr.next.next
+            }
+            
+            curr = curr.next;
+            
+        }
+        
+     return head;
+        
+    };
+
+
+    
