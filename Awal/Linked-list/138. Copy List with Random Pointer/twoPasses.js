2b64b5e33a77737cb01dfcf962b2412864589a74
Awal hossain <waqin21@gmail.com>
2022-12-01 15:14:37 +0600

    copy list with random pointer using two Passes algo 😒

--- Awal/Linked-list/138. Copy List with Random Pointer/twoPasses.js
+++ Awal/Linked-list/138. Copy List with Random Pointer/twoPasses.js
@@ -1,20 +1,23 @@
 var copyRandomList = function(head) {
-    let oldCopy = {};
-     let cur = head;
-     
-     while(cur){
-         let copy = new Node(cur.val);
-         oldCopy[cur] = copy
-         cur =cur.next
-     }
-     
-     cur = head;
-     while(cur){
-         let copy = oldCopy[cur];
-         copy.next = oldCopy[cur.next] || null
-         copy.random = oldCopy[cur.random] || null
-         cur = cur.next
-     }
-     
-     return oldCopy[head]
- };
+    let map = new Map();
+        
+        let curr = head;
+        
+        while(curr){
+            let copy = new Node(curr.val);
+            map.set(curr, copy);
+            curr = curr.next;
+        }
+            curr = head;
+        // console.log(map.get(curr), map.get(curr.random))
+    
+        while(curr){
+            let copy = map.get(curr);
+            copy.next = map.get(curr.next) || null;
+            copy.random = map.get(curr.random)|| null;
+            curr = curr.next;
+        }
+        
+        return map.get(head)
+        
+    };
