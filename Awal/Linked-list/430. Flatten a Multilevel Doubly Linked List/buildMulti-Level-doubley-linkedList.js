c0018cdb3ed685a292358704dabaf98bf0afdbea
Awal hossain <waqin21@gmail.com>
2022-12-02 11:17:32 +0600

    building multilevel linked-list ✌️

--- Awal/Linked-list/430. Flatten a Multilevel Doubly Linked List/buildMulti-Level-doubley-linkedList.js
+++ Awal/Linked-list/430. Flatten a Multilevel Doubly Linked List/buildMulti-Level-doubley-linkedList.js
@@ -16,5 +16,46 @@ const nodes = [1, [2, 7, [8, 10, 11], 9], 3, 4, [5, 12, 13], 6]
 
 const buildMultiLevel = function(nodes){
     const head = new listNode(nodes[0]);
+    console.log(head);
+    let curr = head;
+    
+    for(let i=1; i<nodes.length; i++){
+        let val = nodes[i];
+        // console.log(val);
+
+        let nextNode;
+
+        if(Array.isArray(val)){
+            nextNode = buildMultiLevel(val);
+            curr.child = nextNode;
+            continue;
+        }
+
+        nextNode = new listNode(val);
+        curr.next = nextNode;
+        nextNode.prev = curr;
+        curr = nextNode;
+    }
+
+    return head;
 
 }
+
+
+const printListMulti = head => {
+    if(!head) {
+      return;
+    }
+  
+    console.log(head.val)
+  
+    if(head.child) {
+      printListMulti(head.child);
+    }
+  
+    printListMulti(head.next);
+  }
+
+let multiLinkedList = buildMultiLevel(nodes);
+
+console.log((multiLinkedList))
