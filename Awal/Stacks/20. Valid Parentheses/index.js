bd2ba485992d5e1b13c5c77829c38f427eec75c7
Awal hossain <waqin21@gmail.com>
2022-12-04 21:36:02 +0600

    valid parenthesis complete 🚀🚀

---
+++ Awal/Stacks/20. Valid Parentheses/index.js
@@ -0,0 +1,37 @@
+var isValid = function(s) {
+    const closeToOpen = {
+        ")":"(",
+        "}":"{",
+        "]":"["
+    }
+
+    const stack = [];
+
+    for(const c in s){
+        console.log(closeToOpen[s[c]], s[c]);
+        if(closeToOpen[s[c]]){
+            if(stack && stack[stack.length-1] == closeToOpen[s[c]]){
+                stack.pop();
+           }
+           else{
+            return false
+           }
+            
+        }else{
+            stack.push(s[c]);
+        }
+        
+    }
+
+    if(stack.length == []){
+        return true;
+    }else{
+        return false;
+    }
+    console.log(stack,"chl");
+
+}
+
+
+
+console.log(isValid("()[]{}"));
